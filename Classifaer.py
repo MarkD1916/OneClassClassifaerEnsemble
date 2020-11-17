72b465ae407383c054bb4b7fe70d37c6388ac03a
MarkD <vmakd1916@gmail.com>
2020-11-17 11:15:19 +0300

    Create some classifaers

--- Classifaer.py
+++ Classifaer.py
@@ -1,43 +1,62 @@
-from sklearn.base import TransformerMixin, BaseEstimator
-from scipy.stats import multivariate_normal
+from sklearn.base import TransformerMixin, BaseEstimator,ClassifierMixin
 import numpy as np
-from sklearn.model_selection import GridSearchCV
-from sklearn.cluster import KMeans
-from sklearn import preprocessing
+from sklearn.model_selection import ParameterGrid
+from sklearn.metrics import accuracy_score
+from sklearn.decomposition import PCA
 import matplotlib.pyplot as plt
-from sklearn.grid_search import ParameterGrid
+import warnings
+warnings.filterwarnings("ignore")
 
-
-class OneClassCV(TransformerMixin,BaseEstimator):
+class OneClassCV(TransformerMixin,BaseEstimator,ClassifierMixin):
     def __init__(self,classifaer,parameter):
         self.classifaer = classifaer
         self.parameter = parameter
-
+        self.verbose = True
+        self.clf = None
 
     def putNoiseInData(self, X, count=10):
         arrayNoise = np.zeros(X.shape)
         arrayNoise = np.std(X) * np.random.randn(arrayNoise.shape[0] * count, arrayNoise.shape[1]) + np.mean(X)
-        return arrayNoise
 
-    def selectBestParameterComb(self, X, noiseTest):
-        model = self.classifaer#.fit(X)
-        print (model)
+        noiseTarget = [1]*len(arrayNoise)
+        return arrayNoise, noiseTarget
 
-        for k, v in self.parameter.items():
-            print (k,v)
-            for val in v:
-                clf = model['oneclasscv'].set_params(**{k: val})
-                print(clf)
+    def selectBestParameterComb(self, X, noiseTest,y, noiseY):
+        model = self.classifaer
+        scoreTrain = []
+        scoreNoise = []
+        gridPar = list(ParameterGrid(self.parameter))
+        for par in gridPar:
+            clf = model['oneclasscv'].set_params(**par)
+            clf.fit(X,y)
+            predictOnTrain = clf.predict(X)
+            predictOnNoise = clf.predict(noiseTest)
+            scoreTrain.append(accuracy_score(y,predictOnTrain))
+            scoreNoise.append(accuracy_score(noiseY,predictOnNoise))
+        if self.verbose==True:
+            bestParInd = np.argmax(np.array(scoreTrain)-np.array(scoreNoise))
+            print (gridPar[bestParInd], "- лучшие параметры")
+            print (np.array(scoreTrain)[bestParInd], "- точность на обучении")
+            print(np.array(scoreNoise)[bestParInd], "- точность на тесте")
+            print ((np.array(scoreTrain)-np.array(scoreNoise))[bestParInd], "- разница")
 
+        self.clf = model['oneclasscv'].set_params(**gridPar[bestParInd])
+        # pca = PCA(n_components=2)
+        # pca.fit_transform(np.vstack([X, noiseTest]))
+        # plt.scatter(np.vstack([X, noiseTest])[:len(X), 0], np.vstack([X, noiseTest])[:len(X), 1])
+        # plt.scatter(np.vstack([X, noiseTest])[len(X):, 0], np.vstack([X, noiseTest])[len(X):, 1])
+        # plt.show()
         return
 
     def fit(self, X, y):
-        noiseTest = self.putNoiseInData(X)
-        self.selectBestParameterComb(X,noiseTest)
+        noiseTest, noiseTarget = self.putNoiseInData(X)
+        self.selectBestParameterComb(X,noiseTest,y,noiseTarget)
+        self.clf.fit(X,y)
         return self
 
     def transform(self, X, y=None):
         return
 
-    def predict(self):
-        return
+    def predict(self,X):
+        predict = self.clf.predict(X)
+        return predict
--- main.py
+++ main.py
@@ -5,6 +5,14 @@ from sklearn.preprocessing import StandardScaler
 from Preprocessing import Features
 from Classifaer import OneClassCV
 from sklearn.svm import OneClassSVM
+from sklearn.ensemble import IsolationForest
+from sklearn.neighbors import LocalOutlierFactor
+from GaussianClassifaer import GaussClassifaer
+import numpy as np
+from sklearn.metrics import accuracy_score
+from sklearn.covariance import OAS, MinCovDet,LedoitWolf,EmpiricalCovariance
+
+
 mainDir = '/home/dmv/Desktop/Rat'
 day = ["2020_09_09"] # если пустой массив - выбирает все дни, дни задаются датой как в названии папки с экспериментом
 year = '2020'
@@ -15,13 +23,63 @@ Search.fileSearch()
 jsonData = Search.parsJson()
 L=RatDataLoader(metaDataByRatNum=jsonData,mainDir=mainDir,year=year,rat=rat,verbose=False,day=day,drugs=drugs)
 Data = L.loadData()
-Y_data_train = Data[rat[0]][day[0]]['1']['trainEvent']
-X_data_train = Data[rat[0]][day[0]]['1']['train']
-print (X_data_train.shape, "<<<<<<")
- #OneClassSVM An upper bound on the fraction of training errors and a lower bound of the fraction of support vectors
+# загрузка и предобработка данных
+Y_data_train1 = Data[rat[0]][day[0]]['1']['trainEvent']
+X_data_train1 = Data[rat[0]][day[0]]['1']['train']
+Y_data_test1 = Data[rat[0]][day[0]]['1']['testEvent']
+X_data_test1 = Data[rat[0]][day[0]]['1']['test']
+Y_data_test2 = Data[rat[0]][day[0]]['2']['testEvent']
+X_data_test2 = Data[rat[0]][day[0]]['2']['test']
+Y_data_train2 = Data[rat[0]][day[0]]['2']['trainEvent']
+X_data_train2 = Data[rat[0]][day[0]]['2']['train']
+#Y_data_test2 = Data[rat[0]][day[0]]['2']['trainEvent']
+#X_data_test2 = Data[rat[0]][day[0]]['2']['train']
+Xnoise = np.vstack([X_data_train1[Y_data_train1!=1],X_data_test1[Y_data_test1!=1],X_data_train2[Y_data_train2!=1],X_data_test2[Y_data_test2!=1]])
+Ynoise = np.concatenate([Y_data_train1[Y_data_train1!=1],Y_data_test1[Y_data_test1!=1],Y_data_test2[Y_data_test2!=1],Y_data_train2[Y_data_train2!=1]])
+X_data_train1 = X_data_train1[Y_data_train1==1]
+Y_data_train1 = Y_data_train1[Y_data_train1==1]
+X_data_test1=X_data_test1[Y_data_test1==1]
+Y_data_test1=Y_data_test1[Y_data_test1==1]
+Ynoise[Ynoise!=1]=1
 
-paramGrid={'nu': [1,2,3,4,5,6], 'gamma': [1,2,3,4,5,6]}
-
+#<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+print ("OneClassSVM")
+paramGrid1={'nu':np.arange(0.01,1,0.03), 'gamma': [(10**(x*(-1)))/4 for x in range(1,20)],'kernel':['rbf']}
+model1 =OneClassSVM()
 mem1 = Pipeline([('features', Features(function="fft", band=[[1,3]])),('scaler', StandardScaler()),
-                 ('oneclasscv', OneClassCV(Pipeline([('oneclasscv',OneClassSVM())]),paramGrid))])
-mem1.fit(X_data_train,Y_data_train)
+                 ('oneclasscv', OneClassCV(Pipeline([('oneclasscv',model1)]),paramGrid1))])
+mem1.fit(X_data_train1[:,[5,7,12],:],Y_data_train1)
+print (accuracy_score(Ynoise,mem1.predict(Xnoise[:,[5,7,12],:])), "- точность на веществах")
+#print(mem1.predict(Xnoise[:,[5,7,12],:]))
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+#<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+print ("IsolationForest")
+paramGrid2={'n_estimators':[x for x in range(100,500,50)], 'contamination':np.arange(0.01,0.5,0.03),'random_state':[0]}
+model2 =IsolationForest()
+mem2 = Pipeline([('features', Features(function="fft", band=[[1,3]])),('scaler', StandardScaler()),
+                 ('oneclasscv', OneClassCV(Pipeline([('oneclasscv',model2)]),paramGrid2))])
+mem2.fit(X_data_train1[:,[5,7,12],:],Y_data_train1)
+print (accuracy_score(Ynoise,mem2.predict(Xnoise[:,[5,7,12],:])))
+print(mem2.predict(Xnoise[:,[5,7,12],:]), "- точность на веществах")
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+#<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+print ("Gaussian")
+paramGrid3={'up_bound':np.arange(0.01,0.16,0.01), 'method':[EmpiricalCovariance(),LedoitWolf(),OAS(),MinCovDet(random_state=0),None]}
+model3 =GaussClassifaer()
+mem3 = Pipeline([('features', Features(function="fft", band=[[1,3]])),('scaler', StandardScaler()),
+                 ('oneclasscv', OneClassCV(Pipeline([('oneclasscv',model3)]),paramGrid3))])
+mem3.fit(X_data_train1[:,[5,7,12],:],Y_data_train1)
+print (accuracy_score(Ynoise,mem3.predict(Xnoise[:,[5,7,12],:])), "- точность на веществах")
+#print(mem3.predict(Xnoise[:,[5,7,12],:]))
+
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+print ("LOF")
+paramGrid4={'n_neighbors':[x for x in range(2, 30, 2)], 'contamination':np.arange(0.01, 0.5, 0.03),'novelty':[True]}
+model4 =LocalOutlierFactor()
+mem4 = Pipeline([('features', Features(function="fft", band=[[1,3]])),('scaler', StandardScaler()),
+                 ('oneclasscv', OneClassCV(Pipeline([('oneclasscv',model4)]),paramGrid4))])
+mem4.fit(X_data_train1[:,[5,7,12],:],Y_data_train1)
+print (accuracy_score(Ynoise,mem4.predict(Xnoise[:,[5,7,12],:])), "- точность на веществах")
+#mem4.predict(Xnoise[:,[5,7,12],:])
